eb23cc5d8df0c4d057caeec3b55077568548224c
ogabekbahriddinov663-svg <ogabekbahriddinov663@gmail.com>
2026-08-07 16:50:14 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("FloorRadarXBot ishga tushdi!")
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("FloorRadarXBot ishga tushdi! ✅")
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+
+    print("FloorRadarXBot ishga tushdi!")
+
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
